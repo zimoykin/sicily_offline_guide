25145e9448196b8008d987edb8c42b2af634898f
Dmitry Zimoykin <zimoykin@icloud.com>
2019-12-08 15:00:24 +0300

    Initial Commit

---
+++ sicily_offline_guide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sicily_offline_guide
+//
+//  Created by Дмитрий on 08.12.2019.
+//  Copyright © 2019 DZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
